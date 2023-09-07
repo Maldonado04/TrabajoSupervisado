0635c594fec1c0f38ce1a28de53a6b4eff0d8e0f
Maldonado04 <144158498+Maldonado04@users.noreply.github.com>
2023-09-07 09:45:21 -0600

    Add files via upload

---
+++ TS5-DE_1025323.py
@@ -0,0 +1,21 @@
+#Trabajo supervisado, sección 17
+#07/09/2023
+#Daniel Maldonado 1025323
+#Crear variables tipo String
+#Entrada: Nombre, edad, carrera y carné
+#Procesos principales: Pedir variables y almacenar datos
+#Salida: Mostrar: nombre, edad, carrera y carné
+
+print("Mi segundo programa")
+
+sNombre = str(input("ingrese su nombre"))
+sEdad = str(input("ingrese su edad"))
+sCarrera = str(input("ingrese su carrera"))
+sCarné = str(input("ingrese su carné"))
+
+print("NOMBRE:", sNombre)
+print("EDAD:", sEdad)
+print("CARRERA:", sCarrera)
+print("CARNÉ:", sCarné)
+
+print("Soy",sNombre,"tengo",sEdad,"años y estudio la cerrera de",sCarrera,"Mi número de carné es:",sCarné)
